02501abf12298a4db8d0656e7aeea4675fa6d0c5
Kamalusta <kamalusta@gmail.com>
2024-01-10 06:23:29 -0800

    open and read monty code file

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ * main - give the count arguments
+ * @argv: it is void
+ * @argc: all atributies
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+  FILE *code;
+  char buff[50];
+
+  if (argc != 2)
+    {
+      printf("USAGE: monty file\n");
+      exit(EXIT_FAILURE);
+    }
+  code = fopen(argv[1], "r");
+  if (!code)
+    {
+      printf("Error: Can't open file %s\n", argv[1]);
+      exit(EXIT_FAILURE);
+    }
+  while(fgets(buff, 50, code))
+    {
+      printf("%s\n", buff);
+    }
+  return (0);
+}
