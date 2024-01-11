4ab375b52dbeda023cd00305418853002e649226
Kamalusta <99289704+Kamalusta@users.noreply.github.com>
2024-01-11 15:05:40 +0400

    Update push.c

--- push.c
+++ push.c
@@ -4,10 +4,9 @@ void push (stack_t **stack, int line_number)
 {
   stack_t *node = malloc(sizeof(stack_t));
   node->n = line_number;
-  if ((*stack) != NULL)
-  node->next = *stack;
-  else
-    node->next = NULL;
+  node->next = *stack
   node->prev = NULL;
+  if (*stack)
+      (*stack)->prev = node;
   *stack = node;
 }
