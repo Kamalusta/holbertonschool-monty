d99f800578fa16851b6f075715ddbc6af534418a
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-12 13:34:37 -0800

    push function Betty style and documentation added

--- push.c
+++ push.c
@@ -1,17 +1,24 @@
 #include "monty.h"
+/**
+ * push - pushes given data to the stack
+ *
+ * @stack: stack to store data
+ * @line_number: number of the line
+ */
 
-void push (stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, unsigned int line_number)
 {
-  stack_t *node = malloc(sizeof(stack_t));
-  if (!node)
-    {
-      fprintf(stderr, "Error: malloc failed");
-      exit(EXIT_FAILURE);
-    }
-  node->n = line_number;
-  node->next = *stack;
-  node->prev = NULL;
-  if (*stack)
-    (*stack)->prev = node;
-  *stack = node;
+	stack_t *node = malloc(sizeof(stack_t));
+
+	if (!node)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+	node->n = line_number;
+	node->next = *stack;
+	node->prev = NULL;
+	if (*stack)
+		(*stack)->prev = node;
+	*stack = node;
 }
