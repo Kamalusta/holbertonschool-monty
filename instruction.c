075e55f1eaff88119d8ea7029af32e534f365390
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-12 13:52:38 -0800

    main function Betty style and documentation added

--- instruction.c
+++ instruction.c
@@ -2,37 +2,43 @@
 
 /**
  * instruction - give the count arguments
+ *
+ * @stack: stack to storage data
  * @cmdline: string line
- * 
- * Return: Always 0.
+ * @n: number argument
+ *
+ * Return: void
  */
+
 void instruction(stack_t **stack, char *cmdline, int n)
 {
-  char *token;
-  instruction_t cmds[] = {{"push", push}, {"pall", pall}, {NULL, NULL}};
-  int i = 0;
-  
-  token = strtok(cmdline, " \t\n");
-  while (cmds[i].opcode)
-    {
-      if (strcmp(cmds[i].opcode, token) == 0)
+	char *token;
+	instruction_t cmds[] = {{"push", push}, {"pall", pall}, {NULL, NULL}};
+	int i = 0;
+
+	token = strtok(cmdline, " \t\n");
+	while (cmds[i].opcode)
 	{
-	  token = strtok(NULL, " \t\n");
-	  if (!token)
-	    {
-	      cmds[i].f(stack, n);
-	      break;
-	    }
-	  if (!atoi(token))
-	    {
-	      fprintf(stderr, "L%d: usage: push integer", n);
-	      exit(EXIT_FAILURE);
-	    }
-	  cmds[i].f(stack, atoi(token));
-	  break;
+		if (strcmp(cmds[i].opcode, token) == 0)
+		{
+			token = strtok(NULL, " \t\n");
+			if (!token)
+			{
+				cmds[i].f(stack, n);
+				break;
+			}
+			if (!atoi(token))
+			{
+				fprintf(stderr, "L%d: usage: push integer", n);
+				exit(EXIT_FAILURE);
+			}
+			cmds[i].f(stack, atoi(token));
+			break;
+		}
+		i++;
 	}
-      i++;
-    }
-  /*  printf("L%d: unknown instruction %s", n, token);
-      exit(EXIT_FAILURE); */
- }
+  /*
+   * printf("L%d: unknown instruction %s", n, token);
+   * exit(EXIT_FAILURE);
+   */
+}
--- main.c
+++ main.c
@@ -9,28 +9,29 @@
 
 int main(int argc, char *argv[])
 {
-  FILE *code;
-  char buff[50];
-  int i;
-  stack_t *stack;
-  stack = NULL;
+	FILE *code;
+	char buff[50];
+	int i;
+	stack_t *stack;
 
-  if (argc != 2)
-    {
-      fprintf(stderr, "USAGE: monty file\n");
-      exit(EXIT_FAILURE);
-    }
-  code = fopen(argv[1], "r");
-  if (!code)
-    {
-      fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-      exit(EXIT_FAILURE);
-    }
-  for (i = 1; fgets(buff, 50, code); i++)
-    {
-      instruction(&stack, buff, i);
-    }
-  fclose(code);
-  freed(stack);
-  return (0);
+	stack = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	code = fopen(argv[1], "r");
+	if (!code)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	for (i = 1; fgets(buff, 50, code); i++)
+	{
+		instruction(&stack, buff, i);
+	}
+	fclose(code);
+	freed(stack);
+	return (0);
 }
