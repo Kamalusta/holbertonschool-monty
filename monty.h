de21513a017da2eb9f96f84b3d1dafd20e350b61
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-12 13:55:21 -0800

    monty.h Betty style and documentation added

--- monty.h
+++ monty.h
@@ -15,9 +15,9 @@
  */
 typedef struct stack_s
 {
-  int n;
-  struct stack_s *prev;
-  struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -30,12 +30,12 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-  char *opcode;
-  void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 void instruction(stack_t **stack, char *cmdline, int n);
-void push (stack_t **stack, unsigned int line_number);
-void pall (stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
 void freed(stack_t *stack);
 #endif /*MONTY_H*/
