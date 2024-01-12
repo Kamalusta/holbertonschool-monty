b427b8e5026f794689690e9093389ce7f3e91751
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-12 14:06:37 -0800

    pint function Betty style and documentation added

--- pint.c
+++ pint.c
@@ -1,5 +1,12 @@
 #include "monty.h"
 
+/**
+ * pint - prints last data in the stack
+ *
+ * @stack: stack to store data
+ * @line_number: number of line
+ */
+
 void pint(stack_t **stack, unsigned int line_number)
 {
 	FILE *file = NULL;
@@ -7,7 +14,8 @@ void pint(stack_t **stack, unsigned int line_number)
 
 	if (*stack)
 		printf("%d\n", (*stack)->n);
-	else {
+	else
+	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		fclose(file);
 		free_stack(stack);
@@ -15,4 +23,3 @@ void pint(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 }
-		
