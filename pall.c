1475d1ed4257fb130e4e8cf0c2c94acaa5547e03
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-12 13:30:13 -0800

    pall function Betty style and documentation added

--- pall.c
+++ pall.c
@@ -1,14 +1,21 @@
 #include "monty.h"
 
-void pall (stack_t **stack, unsigned int line_number)
+/**
+ * pall - prints all stack data
+ *
+ * @stack: stack to store data
+ * @line_number: number of the line
+ */
+
+void pall(stack_t **stack, unsigned int line_number)
 {
-  stack_t *node;
-  (void)line_number;
-  
-  node = *stack;
-  while(node)
-    {
-      printf("%d\n", node->n);
-      node = node->next;
-    }
+	stack_t *node;
+	(void)line_number;
+
+	node = *stack;
+	while (node)
+	{
+		printf("%d\n", node->n);
+		node = node->next;
+	}
 }
