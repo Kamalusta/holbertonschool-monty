fe6306b1ee0c4612cca601588f6ca9b5341a148e
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-12 13:04:49 -0800

    add function added

--- monty.h
+++ monty.h
@@ -41,5 +41,5 @@ void pint(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t **stack);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
-
+void add(stack_t **stack, unsigned int line_number);
 #endif /*MONTY_H*/
