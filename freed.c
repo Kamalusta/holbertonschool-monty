074cad793ca32b9dbc1a41cd0c54c39c47ebef1d
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-12 13:39:15 -0800

    freed function Betty style and documentation added

--- freed.c
+++ freed.c
@@ -1,13 +1,19 @@
 #include "monty.h"
 
+/**
+ * freed - free to deallocate memory when needed
+ *
+ * @stack: stack argument to free when needed
+ */
+
 void freed(stack_t *stack)
 {
-  stack_t *current;
+	stack_t *current;
 
-  while (stack)
-    {
-      current = stack;
-      stack = stack->next;
-      free(current);
-    }
+	while (stack)
+	{
+		current = stack;
+		stack = stack->next;
+		free(current);
+	}
 }
