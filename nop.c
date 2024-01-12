64902aa5ecb64aae250b5c1460a5ceeac566114d
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-12 14:03:20 -0800

    nop function Betty style and documentation added

--- nop.c
+++ nop.c
@@ -1,5 +1,12 @@
 #include "monty.h"
 
+/*
+ * nop - The opcode nop doesn’t do anything.
+ *
+ * @stack: stack to store data
+ * @line_nubers: number of line
+ */
+
 void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
