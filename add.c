66fb19334e9c0e70467281ead422f3a8ccaaf79b
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-13 05:06:23 -0800

    betty style and doc for add

--- add.c
+++ add.c
@@ -1,11 +1,18 @@
 #include "monty.h"
 
+/**
+ * add -adds the top two elements of the stack.
+ *
+ * @stack: stack to store data
+ * @line_number: line number
+ */
+
 void add(stack_t **stack, unsigned int line_number)
 {
 	FILE *file = NULL;
 	char *line = NULL;
 
-	if (*stack && (*stack)->next) 
+	if (*stack && (*stack)->next)
 	{
 		(*stack)->next->n += (*stack)->n;
 		pop(stack, line_number);
